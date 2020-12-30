c3856347d0156f84346b071b6b5b2a3f2206d8b3
David Harlow <davidh_95@hotmail.com>
2020-12-30 20:46:19 +0000

    clean slate

--- sam-harlow/src/layouts/Default.vue
+++ sam-harlow/src/layouts/Default.vue
@@ -5,8 +5,6 @@
         <g-link to="/">{{ $static.metadata.siteName }}</g-link>
       </strong>
       <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
       </nav>
     </header>
     <slot/>
@@ -27,10 +25,12 @@ body {
   margin:0;
   padding:0;
   line-height: 1.5;
+  background-color: black;
+  color: white;
 }
 
 .layout {
-  max-width: 760px;
+  max-width: 1000px;
   margin: 0 auto;
   padding-left: 20px;
   padding-right: 20px;
