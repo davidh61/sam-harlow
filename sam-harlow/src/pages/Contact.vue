d763599d463d090d6ddb7f13f895a4b663ae5492
David Harlow <davidh_95@hotmail.com>
2021-02-02 14:11:40 +0000

    added flexyform

--- sam-harlow/src/pages/Contact.vue
+++ sam-harlow/src/pages/Contact.vue
@@ -1,14 +1,28 @@
 <template>
   <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <h1>Contact</h1>
+
+    <form
+      method="post"
+      action="https://www.flexyform.com/f/d1692555237541bb809b2009e477d86c693ab83c"
+    >
+      <p>
+        <label>Fullname</label>
+        <input type="text" name="fullname" />
+      </p>
+      <p>
+        <label>Email</label>
+        <input type="email" name="_reply_to" />
+      </p>
+      <button type="submit">Send</button>
+    </form>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'About us'
-  }
-}
+    title: "Contact",
+  },
+};
 </script>
