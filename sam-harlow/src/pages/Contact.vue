28fb6c9b56ed28e088557f6f442a61319da34c89
David Harlow <davidh_95@hotmail.com>
2021-02-06 14:33:54 +0000

    initial form styling

--- sam-harlow/src/pages/Contact.vue
+++ sam-harlow/src/pages/Contact.vue
@@ -6,15 +6,15 @@
         action="https://www.flexyform.com/f/d1692555237541bb809b2009e477d86c693ab83c"
       >
         <div>
-          <input type="text" placeholder="Name" name="fullname" />
+          <input type="text" placeholder="Name" name="fullname" required />
         </div>
         <div>
-          <input type="email" placeholder="Email" name="_reply_to" />
+          <input type="email" placeholder="Email" name="_reply_to" required />
         </div>
         <div>
-          <textarea name="message" placeholder="Message"> </textarea>
+          <textarea name="message" placeholder="Message" required > </textarea>
         </div>
-        <button type="submit">Send</button>
+        <button type="submit">SEND</button>
       </form>
     </div>
   </Layout>
@@ -34,12 +34,34 @@ export default {
   justify-content: space-between;
   justify-content: center;
 
-  .form {
+  form {
+    max-width: 400px;
+    width: 100%;
 
-    .input {
-
+    input,
+    textarea,
+    button {
+      resize: none;
+      width: 100%;
+      box-sizing: border-box;
+      padding: 12px 20px;
+      margin: 8px 0;
+      border: 2px solid #2a2a2a;
+      border-radius: 4px;
     }
 
+    textarea {
+      height: 100px;
+    }
+
+    button {
+      color: white;
+      background-color: #2a2a2a;
+
+      &:hover {
+        cursor: pointer;
+      }
+    }
   }
 }
 </style>
